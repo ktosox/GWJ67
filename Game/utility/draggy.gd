c44e7227a0b92c339b1ea9202a201b8d6826d21f
Ktosox <ktoso3@gmail.com>
2024-03-11 11:59:42 +0100

    added some placeholder scenes for game flow + fine tunning dragging

--- Game/utility/draggy.gd
+++ Game/utility/draggy.gd
@@ -12,18 +12,26 @@ func _physics_process(delta):
 	
 	if drag:
 		
-		$GrabbyPart.global_position = $GrabbyPart.global_position.move_toward(get_global_mouse_position(),delta *$GrabbyPart.global_position.distance_squared_to(get_global_mouse_position()) )
-
+		$GrabbyPart.global_position = get_global_mouse_position()
+		$TractorBeam.points[1] = to_local(get_global_mouse_position())
+		$TractorBeam.width = 26 - ($GrabbyPart.global_position.distance_to(global_position) * 0.2)
+		if $TractorBeam.width < 2:
+			drag_ended()
 		
+		
+
+func drag_ended():
+	emit_signal("dragging",false)
+	drag = false
+	$TractorBeam.points[1] = Vector2.ZERO
+	node_a = NodePath()
+	$GrabbyPart.global_position = global_position
+	pass
 
 func _input(event):
 	if event.is_class("InputEventMouseButton") and drag:
 		if event.button_index == 1 and !event.is_pressed():
-			emit_signal("dragging",false)
-			drag = false
-			bias = 0.9
-			$GrabbyPart.global_position = global_position
-			
+			drag_ended()
 			if(!$MouseDetector.get_rect().has_point(get_local_mouse_position())):
 				yield(get_tree().create_timer(0.05), "timeout")
 				_on_MouseDetector_mouse_exited()
@@ -35,7 +43,7 @@ func _on_MouseDetector_gui_input(event):
 	if event.is_class("InputEventMouseButton") and !drag :
 		if event.button_index == 1 and event.is_pressed():
 			drag = true
-			bias = 0.4
+			node_a = $GrabbyPart.get_path()
 			emit_signal("dragging",true)
 
 	pass # Replace with function body.
---
+++ Game/world/welcome_screen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func start_game():
+	print("switch to day 1 introduction")
+	pass
+
+
+func _on_Button_pressed():
+	start_game()
+	pass # Replace with function body.
