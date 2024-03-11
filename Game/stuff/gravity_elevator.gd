e8e1371ec022f1e213678d34ad4054e4e5b9d544
Ktosox <ktoso3@gmail.com>
2024-03-11 13:27:49 +0100

    added first version of gravity elevator

---
+++ Game/stuff/gravity_elevator.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func flip_gravivty():
+	var new_direction = gravity_vec.y * -1
+	gravity_vec.y = new_direction
+	$Beam.material.set("shader_param/scorll1",Vector2(0,new_direction * -0.385))
+	$Beam.material.set("shader_param/scorll2",Vector2(0,new_direction * -0.45))
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	flip_gravivty()
+	pass # Replace with function body.
