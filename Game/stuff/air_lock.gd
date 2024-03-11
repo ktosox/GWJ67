a8006c63cd279698ce334307413b0c6cf5fbe975
Ktosox <ktoso3@gmail.com>
2024-03-11 13:53:08 +0100

    added air lock + ground work for deleting cargo

---
+++ Game/stuff/air_lock.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func door_error():
+	print("error: cant open both doors")
+	pass
+
+func _on_Right_pressed():
+	if !$DoorLeft.visible:
+		door_error()
+		return
+	$DoorRight.visible = !$DoorRight.visible
+	$DoorRight/Collider.disabled = !$DoorRight/Collider.disabled
+	if !$DoorRight.visible:
+		$VacumOfSpace.space_override = Area2D.SPACE_OVERRIDE_REPLACE
+	else:
+		$VacumOfSpace.space_override = Area2D.SPACE_OVERRIDE_DISABLED
+	pass # Replace with function body.
+
+
+func _on_Left_pressed():
+	if !$DoorRight.visible:
+		door_error()
+		return
+	$DoorLeft.visible = !$DoorLeft.visible
+	$DoorLeft/Collider.disabled = !$DoorLeft/Collider.disabled
+	pass # Replace with function body.
