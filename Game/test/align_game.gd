ea708b2545a84fd03695650d292f5a6fa786ce2a
Ktosox <ktoso3@gmail.com>
2024-03-21 22:36:38 +0100

    started work on ship asset + made new version of ring game

--- Game/test/align_game.gd
+++ Game/test/align_game.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
-export var in_speed = 0.4
-export var mid_speed = -0.4
-export var out_speed = 0.8
+var in_speed = [1.8,1.8]
+var mid_speed = [1.8,1.8]
+var out_speed = [1.8,1.8]
+
+var current_segment = 0
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,7 +19,56 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$center/OutRing.rotation += delta * out_speed
-	$center/MiddleRing.rotation += delta * mid_speed
-	$center/InnerRing.rotation += delta * in_speed
+	$center/OutRing.rotation += delta * out_speed[0]
+	$center/MiddleRing.rotation += delta * mid_speed[0]
+	$center/InnerRing.rotation += delta * in_speed[0]
+
 	pass
+
+func swap_segment():
+
+	out_speed[0] = out_speed[1]
+	in_speed[0] = in_speed[1]
+	mid_speed[0] = mid_speed[1]
+	current_segment = (current_segment+1)%3
+	$center/OutRing.modulate = ColorN("White")
+	$center/InnerRing.modulate = ColorN("White")
+	$center/MiddleRing.modulate = ColorN("White")
+	match current_segment:
+		0:
+			
+			$center/InnerRing.modulate = ColorN("Pink")
+			pass
+		1:
+			
+			$center/MiddleRing.modulate = ColorN("Pink")
+			pass
+		2:
+			
+			$center/OutRing.modulate = ColorN("Pink")
+			pass
+	pass
+
+func press_segment():
+	print("press")
+	match current_segment:
+		0:
+			in_speed[0] = 0.0
+		1:
+			mid_speed[0] = 0.0
+		2:
+			out_speed[0] = 0.0
+
+func _input(event:InputEvent):
+	if event.is_action("ui_accept"):
+		if event.is_pressed() and Input.is_action_just_pressed("ui_accept"):
+			
+			press_segment()
+		elif Input.is_action_just_released("ui_accept"):
+			swap_segment()
+
+
+func _on_Line_area_entered(area):
+	if $center/MiddleRing/Line.get_overlapping_areas().size() == 2:
+		print("win")
+	pass # Replace with function body.
